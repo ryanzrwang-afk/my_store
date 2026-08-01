d2f4847fdd80b151b265686cc4732eba3099472d
Ryan Wang <ryan.zrwang@gmail.com>
2026-08-01 11:15:04 -0500

    User input

---
+++ python/hw_two.py
@@ -0,0 +1,22 @@
+# === unit1_ex3 ===
+# Exercise 3: Print a product catalog and try using newline \n
+
+
+
+# === unit1_ex2 ===
+# Exercise 2: Use the print function to display your store's name
+
+# Please enter your code below.
+
+
+# === unit1_hw2 ===
+# Homework 2: Use Python to display your designed store
+
+# ✅ Requirements:
+# - Print store name
+# - Print product list (one item per line)
+# ⭐ Bonus: Add special symbols or decorations to make it look nice!
+
+# Example:
+print("☆✼★ Welcome to my Store ★✼☆")
+print("Mcdonalds\n Chicken\n Door\n yummy chips\n Chair\n Microphone")
---
+++ python/hw_unit_2.py
@@ -0,0 +1,12 @@
+# 📝 Homework 1: Update your store program
+# Requirements: Add order input feature to your previous store program
+# ✅ Use input to ask: What do you want to buy?
+# ✅ Use input to ask: How many do you want?
+# ✅ Save both answers to variables
+# ✅ Use print to show the order
+# ✅ Add comments for each line
+
+# Continue your code below...
+ask = input("what do you wanna buy? ")
+num = input("how many do you want? ")
+print("User wants to buy: " + num + " " + ask)
